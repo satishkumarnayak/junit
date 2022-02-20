f4120c5771b3dd8c5524cfe47e59245bba43a9fa
Satish Nayak <satish_nayak@epam.com>
2022-02-20 08:06:43 +0530

    junit test

---
+++ src/test/java/com/instanaceofcake/junit/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.instanaceofcake.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
